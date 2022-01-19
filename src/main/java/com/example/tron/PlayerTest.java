124f4398168ee0ce3087c1ae44ef74238ad4a329
KMeska <kristijonas.meska@stud.vgtu.lt>
2022-01-19 17:11:28 +0200

    nr1

--- src/main/java/com/example/tron/PlayerTest.java
+++ src/main/java/com/example/tron/PlayerTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class PlayerTest {
 
     @org.junit.jupiter.api.Test
-    void test1() {
+    void PlayerHuman_StepOnUsedTile_IsDead() {
         Map map = new Map();
         PlayerHuman pl = new PlayerHuman(1, 1, "left", "xxxx", 2, map);
         pl.behave();
@@ -13,7 +13,7 @@ class PlayerTest {
     }
 
     @org.junit.jupiter.api.Test
-    void test2() {
+    void PlayerHuman_StepOnFreeTile_IsAlive {
         Map map = new Map();
         PlayerHuman pl = new PlayerHuman(1, 1, "right", "xxxx", 2, map);
         pl.behave();
@@ -21,7 +21,7 @@ class PlayerTest {
     }
 
     @org.junit.jupiter.api.Test
-    void test3() {
+    void PlayerHuman_StepOnUsedTile_NotPossible() {
         Map map = new Map();
         map.drawOnMap(1, 2, 3);
         PlayerHuman pl = new PlayerHuman(1, 1, "right", "xxxx", 2, map);
@@ -30,10 +30,12 @@ class PlayerTest {
     }
 
     @org.junit.jupiter.api.Test
-    void test4() {
+    void PlayerHuman_StepOnFreeTile_Possible() {
         Map map = new Map();
         PlayerHuman pl = new PlayerHuman(1, 1, "right", "xxxx", 2, map);
+
         pl.behave();
-        assertEquals(2, map.getElement(1, 2));
+
+        assertEquals(2, map.getElement(2, 1));
     }
 }
